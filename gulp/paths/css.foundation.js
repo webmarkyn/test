84c93f083b2383ef3aa5b0a5b5109cf2ccd07242
Mark Baidebura <web.markyn@gmail.com>
2016-11-30 20:03:23 +0300

    for test

--- gulp/paths/css.foundation.js
+++ gulp/paths/css.foundation.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = [
-  './node_modules/normalize.css/normalize.css'
+  './node_modules/normalize.css/normalize.css',
+  './node_modules/font-awesome/css/font-awesome.min.css'
 ];
--- source/js/app.js
+++ source/js/app.js
@@ -4,4 +4,5 @@
   setTimeout(function() {
     document.querySelector('.greating_picture').classList.add('m--show');
   }, 1000);
+  
 })();
